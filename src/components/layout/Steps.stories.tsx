78c8bb6bf0ac288ec0baf1ee38a7af728823dac7
Hugo Cox <h.n.c.cox@gmail.com>
2022-09-06 19:10:18 +0200

    grad

--- src/components/layout/Steps.stories.tsx
+++ src/components/layout/Steps.stories.tsx
@@ -1,5 +1,6 @@
-import React, {useRef, useState} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import * as React from 'react';
+import {StyleSheet, View, Text, ScrollView} from 'react-native';
+import { useSwipe } from '../../hooks/useSwipe'
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 
 import tw from 'twrnc';
@@ -16,24 +17,38 @@ export const Default: ComponentStory<typeof Steps> = () => {
 
 	const totalSteps = 5
 	const loop = false;
-	const [currentStep, setCurrentStep] = useState(1)	// get from store
+	let currentStep = 1
+	// const [currentStep, setCurrentStep] = React.useState(1)	// get from store
 
+	const { onTouchStart, onTouchEnd } = useSwipe(onSwipeLeft, onSwipeRight, 6)
+
+	function onSwipeLeft(){
+		console.log('SWIPE_LEFT')
+	}
+
+	function onSwipeRight(){
+			console.log('SWIPE_RIGHT')
+	}
 	const onPress = (page: string) => {
 		switch (page) {
 			case 'prev':
 				if(currentStep === 1) {
 					if(!loop) return
-					setCurrentStep(totalSteps)
+					// setCurrentStep(totalSteps)
+					currentStep = totalSteps
 				} else {
-					setCurrentStep(currentStep - 1)
+					currentStep = currentStep - 1
+					// setCurrentStep(currentStep - 1)
 				}
 				break;
 			case 'next':
 				if(currentStep === totalSteps) {
 					if(!loop) return
-					setCurrentStep(1)
+					// setCurrentStep(1)
+					currentStep = 1
 				} else {
-					setCurrentStep(currentStep + 1)
+					// setCurrentStep(currentStep + 1)
+					currentStep = currentStep + 1
 				}
 				break;
 		}
@@ -41,7 +56,7 @@ export const Default: ComponentStory<typeof Steps> = () => {
 
 	return (
 		<View style={tw`m-auto`}>
-			<View style={tw`flex flex-1 flex-row justify-center items-center`}>
+			<View onTouchStart={onTouchStart} onTouchEnd={onTouchEnd} style={tw`flex flex-1 flex-row justify-center items-center`}>
 				<AppButton onPress={() => onPress('prev')} buttonType='default' title="<" />
 				<Steps totalSteps={totalSteps} currentStep={currentStep} />
 				<AppButton onPress={() => onPress('next')} buttonType='default' title=">" />
